587d990a895c499f97a78bb77995cf2745ddb37e
Freddy Cruz Estrada <freddy.cruze@gmail.com>
2026-04-05 13:27:43 -0600

    Temporarily remove CORS errors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Local dev: Angular dev server (see sportflow frontend).
+  app.enableCors({
+    origin: ['http://localhost:4200', 'http://127.0.0.1:4200'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
